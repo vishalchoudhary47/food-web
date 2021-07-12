79b544756fb4122c5c215645d92d0983e16dea01
vishal <vishalchoudhary80599@gmail.com>
2021-07-12 19:28:18 +0530

    last commit

--- menu-js.js
+++
@@ -1,9 +0,0 @@
-const navSlide = () => {
-	const bar = document.querySelector('.bar');
-	const nav = document.querySelector('.nav-links');
-	//Active Toggle
-	bar.addEventListener('click',()=>{
-		nav.classList.toggle('bar-click');
-	});
-}
-navSlide();
